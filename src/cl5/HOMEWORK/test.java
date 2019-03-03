ab9ac429a3df5a96a6955d0cab49f66a02e32704
Vlad Zinkovsky <terraemw@gmail.com>
2019-03-03 14:13:35 +0200

    add init

---
+++ src/cl5/HOMEWORK/test.java
@@ -0,0 +1,11 @@
+package cl5.HOMEWORK;
+
+public class test {
+    public static void main(String[] args) {
+        IntList list = new IntLinkedList();
+        list.add(10);
+        list.add(20);
+
+        System.out.println(list);
+    }
+}

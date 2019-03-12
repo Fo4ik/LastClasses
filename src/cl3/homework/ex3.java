74c668270268688c3c1fa1dbaf3b00ad5c1cf78e
Fo4ik <terraemw@gmail.com>
2019-03-12 22:40:45 +0200

    Update ex3.java

--- src/cl3/homework/ex3.java
+++ src/cl3/homework/ex3.java
@@ -1,16 +1,37 @@
 package cl3.homework;
 
 public class ex3 {
-    public static void main(String[] args) {
-        int array [][]= {{0,0,1} ,
-                       {0,1,0} ,
-                       {1,0,0}};
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                array[i][j] = (array[i][j] == 1) ? 0 : 1;
-                System.out.print(array[i][j] + " ");
+   public static void main(String[] args) {
+
+        int[][] arr = {
+                {0, 1, 0},
+                {1, 1, 0},
+                {0, 0, 1},
+                {0, 1, 1}
+        };
+
+        arrayToString(arr);
+        System.out.println();
+        arrayToString(changeArray(arr));
+    }
+
+    private static int[][] changeArray(int[][] arr) {
+        int[][] tmp = new int[arr.length][arr[0].length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][arr[i].length - 1 - j] == 1) {
+                    tmp[i][j] = 0;
+                } else {
+                    tmp[i][j] = 1;
+                }
             }
-            System.out.println();
+        }
+        return tmp;
+    }
+
+    private static void arrayToString(int[][] arr) {
+        for (int[] ints : arr) {
+            System.out.println(Arrays.toString(ints));
         }
     }
 }
